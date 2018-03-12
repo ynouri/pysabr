9f796122f4f392169385f60917adb6d2b3c1cdf6
ynouri <ynouri@users.noreply.github.com>
2018-03-12 17:44:56 -0400

    More unit tests (passing)

--- tests/black_test.py
+++ tests/black_test.py
@@ -1,21 +1,39 @@
 from sabr import black_lognormal_call
 import numpy as np
 import logging
+import pytest
+from pytest import approx
 
-def test_intrinsic_value_on_expiry():
-    [k, f, t, v, r, cp] = [2., 3., 0., 0.20, 0.02, 'call']
-    pv = black_lognormal_call(k, f, t, r, v, cp) * 1e5
-    logging.debug("PV = {}".format(pv))
-    assert pv == 1e5
+ERROR_TOLERANCE = 0.001 # 0.1% error is tolerated
 
-def test_far_itm_call():
-    [k, f, t, v, r, cp] = [2., 10., 1.5, 0.60, 0.03, 'call']
-    pv = black_lognormal_call(k, f, t, r, v, cp) * 1e5
-    logging.debug("PV = {}".format(pv))
-    assert pv == 8. * np.exp(-0.03*1.5)
+test_data = {
+    '1y6m 800bps itm call 10% vol': [
+        1e5,
+        [2., 10., 1.5, 0.10, 0.03, 'call'],
+        1e5 * (10. - 2.) * np.exp(-0.03*1.5)
+    ],
+    '10y 10bps itm call 20% vol': [
+        1e5,
+        [0.012, 0.013, 10., 0.20, 0.02, 'call'],
+        296.88
+    ],
+    '6m 20bps otm put 50% vol': [
+        1e5,
+        [0.030, 0.028, 0.5, 0.50, 0.04, 'put'],
+        504.37
+    ],
+    '2y atm put 100% vol': [
+        1e5,
+        [0.005, 0.005, 2., 1., 0.10, 'put'],
+         213.07
+    ]
+}
 
-def test_call_pv():
-    [k, f, t, v, r, cp] = [0.012, 0.013, 10., 0.20, 0.02, 'call']
-    pv = black_lognormal_call(k, f, t, r, v, cp) * 1e5
+@pytest.mark.parametrize("notional, option_data, target_pv",
+                         test_data.values(), ids=list(test_data.keys()))
+def test_black_lognormal_pv(notional, option_data, target_pv):
+    [k, f, t, v, r, cp] = option_data
+    pv = notional * black_lognormal_call(k, f, t, v, r, cp)
     logging.debug("PV = {}".format(pv))
-    assert pv == 0.
+    logging.debug("Target PV = {}".format(target_pv))
+    assert pv == approx(target_pv, ERROR_TOLERANCE)
