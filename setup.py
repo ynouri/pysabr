dc86624bf01db9f2c9a35261ad5d741b4153568a
ynouri <ynouri@users.noreply.github.com>
2018-03-15 22:13:27 -0400

    version 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='pysabr',
     description='SABR volatility model for interest rates options',
     url='https://github.com/ynouri/pySABR',
-    version='0.1.0',
+    version='0.1.3',
     license='MIT',
     author='Yacine Nouri',
     packages=['sabr', 'black'],
