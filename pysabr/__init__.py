54cdaecc409012ed05cd54177378a29c374b0ab6
ynouri <ynouri@users.noreply.github.com>
2018-05-22 17:23:02 -0400

    Normal SABR

--- pysabr/__init__.py
+++ pysabr/__init__.py
@@ -2,7 +2,11 @@
 
 from .models import hagan_2002_lognormal_sabr
 from .models.hagan_2002_lognormal_sabr import Hagan2002LognormalSABR
+from .models import hagan_2002_normal_sabr
+from .models.hagan_2002_normal_sabr import Hagan2002NormalSABR
 
 
 __all__ = ["hagan_2002_lognormal_sabr",
-           "Hagan2002LognormalSABR"]
+           "Hagan2002LognormalSABR",
+           "hagan_2002_normal_sabr",
+           "Hagan2002NormalSABR"]
--- pysabr/models/hagan_2002_normal_sabr.py
+++ pysabr/models/hagan_2002_normal_sabr.py
@@ -1,5 +1,62 @@
-from base_sabr import BaseNormalSABR
+from .base_sabr import BaseNormalSABR
+import numpy as np
 
 
 class Hagan2002NormalSABR(BaseNormalSABR):
-    pass
+
+    def alpha(self):
+        """Implies alpha parameter from the ATM normal volatility."""
+        return NotImplementedError("To be implemented")
+
+    def normal_vol(self, k, alpha):
+        """Return normal volatility for a given strike."""
+        f, s, t = self.f, self.shift, self.t
+        beta, rho, volvol = self.beta, self.rho, self.volvol
+        v_n = normal_vol(k+s, f+s, t, alpha, beta, rho, volvol)
+        return v_n
+
+    def fit(self, k, v_sln):
+        """Calibrate SABR parameters alpha, rho and volvol."""
+        raise NotImplementedError("To be implemented")
+
+
+def normal_vol(k, f, t, alpha, beta, rho, volvol):
+    """Hagan's 2002 SABR normal vol expansion - formula (B.67a)."""
+    # We break down the complex formula into simpler sub-components
+    f_av = np.sqrt(f / k)
+    A = - beta * (2 - beta) * alpha**2 / (24 * f_av**(2 - 2 * beta))
+    B = rho * alpha * volvol * beta / (4 * f_av**(1 - beta))
+    C = (2 - 3 * rho**2) * volvol**2 / 24
+    FMKR = _f_minus_k_ratio(f, k, beta)
+    ZXZ = _zeta_over_x_of_zeta(k, f, t, alpha, beta, rho, volvol)
+    # Aggregate all components into actual formula (B.67a)
+    v_n = alpha * FMKR * ZXZ * (1 + (A + B + C) * t)
+    return v_n
+
+
+def _f_minus_k_ratio(f, k, beta):
+    """Hagan's 2002 f minus k ratio - formula (B.67a)."""
+    eps = 1e-07  # Numerical tolerance for f-k
+    if abs(f-k) > eps:
+        return (1 - beta) * (f - k) / (f**(1-beta) - k**(1-beta))
+    else:
+        return k**beta
+
+
+def _zeta_over_x_of_zeta(k, f, t, alpha, beta, rho, volvol):
+    """Hagan's 2002 zeta / x(zeta) function - formulas (B.67a)-(B.67b)."""
+    eps = 1e-07  # Numerical tolerance for zeta
+    f_av = np.sqrt(f / k)
+    zeta = volvol * (f - k) / (alpha * f_av**beta)
+    if abs(zeta) > eps:
+        return zeta / _x(rho, zeta)
+    else:
+        # The ratio converges to 1 when zeta approaches 0
+        return 1.
+
+
+def _x(rho, z):
+    """Hagan's 2002 x function - formula (B.67b)."""
+    a = (1 - 2*rho*z + z**2)**.5 + z - rho
+    b = 1 - rho
+    return np.log(a / b)
