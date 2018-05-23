23259b8ce04a66314adcc229fd760fa51e560321
ynouri <ynouri@users.noreply.github.com>
2018-05-23 00:18:07 -0400

    Test premium

--- pysabr/tests/hagan_2002_lognormal_sabr/test_cube.py
+++ pysabr/tests/hagan_2002_lognormal_sabr/test_cube.py
@@ -18,14 +18,13 @@ def test_vols(vol_cube):
     assert vols_test == pytest.approx(vols_target.values, MAX_ERROR)
 
 
-# TODO, premiums are available in premiums.csv
 def test_premiums(vol_cube):
     """Test the premiums."""
     (f, s, t, v_atm_n, beta, rho, volvol), df, _, premiums_target = vol_cube
     sabr = Hagan2002LognormalSABR(f/100, t, s/100, v_atm_n/1e4,
                                   beta, rho, volvol)
     strikes = premiums_target.index[premiums_target.index + s > 0.]
-    premiums_test = [df * sabr.call(k/100) * 1e4 for k in strikes]
-    premiums_target = premiums_target[strikes].values * 1e4
-    #assert premiums_test == pytest.approx(premiums_target, MAX_ERROR)
-    assert True
+    # TODO: strikes = premiums_target.index
+    premiums_test = [sabr.call(k/100) * 100 for k in strikes]
+    premiums_target = premiums_target[strikes].values
+    assert premiums_test == pytest.approx(premiums_target, MAX_ERROR)
