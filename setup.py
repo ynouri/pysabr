438cf429088620e3ff0873a5462f07acaae93aa5
ynouri <ynouri@users.noreply.github.com>
2018-05-24 19:53:12 -0400

    Detect all packages

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 setup(
     name='pysabr',
     description='SABR volatility model for interest rates options',
     url='https://github.com/ynouri/pysabr',
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     author='Yacine Nouri',
-    packages=['pysabr', 'web'],
+    packages=find_packages(),
     python_requires='>=3',
     install_requires=['numpy', 'scipy', 'falcon']
 )
