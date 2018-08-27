76ef5047f9c0c85fa82ea9dc298ab6a70de9a7ce
ynouri <ynouri@users.noreply.github.com>
2018-08-27 17:34:00 -0400

    Increase version to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pysabr',
     description='SABR volatility model for interest rates options',
     url='https://github.com/ynouri/pysabr',
-    version='0.2.1',
+    version='0.3.0',
     license='MIT',
     author='Yacine Nouri',
     packages=find_packages(),
