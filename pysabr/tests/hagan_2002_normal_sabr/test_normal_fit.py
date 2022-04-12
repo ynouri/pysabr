ac91e6c53778e43a75ca6f531dda7ff5f22e546e
Vladimir Tsepelev <v.v.tsepelev@gmail.com>
2022-04-12 21:15:32 +0300

    unit test for normal model fit method

--- pysabr/tests/hagan_2002_normal_sabr/test_normal_fit.py
+++ pysabr/tests/hagan_2002_normal_sabr/test_normal_fit.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 12 15:13:43 2022
-
-@author: hasek
-"""
-
 import numpy as np
 from pysabr import Hagan2002NormalSABR
 import logging
